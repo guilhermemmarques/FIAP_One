30aa003973423d0b11728535944c7b9f088464ae
juboldrin <boldrin_julia@hotmail.com>
2022-09-26 16:53:29 -0300

    Incluido classe DAL e Controller

---
+++ FiapSmartCity/FiapSmartCity/Controllers/BuscaEnderecoController.cs
@@ -0,0 +1,34 @@
+﻿using FiapSmartCity.DAL;
+using FiapSmartCity.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FiapSmartCity.Controllers
+{
+
+    [ApiController]
+    [Route("busca-endereco")]
+    public class BuscaEnderecoController : ControllerBase
+    {
+        private readonly BuscaEnderecoDAL buscaEnderecoDAL;
+
+        public BuscaEnderecoController()
+        {
+            buscaEnderecoDAL = new BuscaEnderecoDAL();
+        }
+
+        [HttpGet("{DescEnderecos:string}")]
+        public ActionResult <Endereco> Get([FromRoute] string DescEnderecos)
+        {
+            try
+            {
+                Endereco endereco = buscaEnderecoDAL.Consultar(DescEnderecos);
+                return Ok(endereco);
+            } catch (KeyNotFoundException e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ FiapSmartCity/FiapSmartCity/DAL/BuscaEnderecoDAL.cs
@@ -0,0 +1,48 @@
+﻿using FiapSmartCity.Models;
+
+namespace FiapSmartCity.DAL
+{
+    public class BuscaEnderecoDAL
+    {
+
+        //Lista para armazenar enderecos cadastrados no banco
+        private static Dictionary<long, Endereco> bdBuscaEnderecos = new Dictionary<long, Endereco>();
+        private static int contadorBanco = 1;
+
+        //Simulando banco de dados
+        static BuscaEnderecoDAL()
+        {
+            Endereco PresidenteJoseLinhares = new Endereco();
+            PresidenteJoseLinhares.IdEndereco = 345;
+            PresidenteJoseLinhares.DescEnderecos = "Presidente José Linhares, 234";
+            PresidenteJoseLinhares.CEP = 09345654;
+            PresidenteJoseLinhares.UF = "SP";
+
+            bdBuscaEnderecos.Add(1, PresidenteJoseLinhares);
+
+        }
+        
+        public void Inserir(Endereco Endereco)
+        {
+            contadorBanco++;
+            Endereco.IdEndereco = contadorBanco;
+            bdBuscaEnderecos.Add(contadorBanco, Endereco);
+        }
+
+        public Endereco Consultar( string DescEnderecos)
+        {
+            return bdBuscaEnderecos[DescEnderecos];
+        }
+
+        public IList<Endereco> Listar()
+        {
+            return new List<Endereco>(bdBuscaEnderecos.Values);
+        }
+
+        public void Excluir(int IdEndereco)
+        {
+            bdBuscaEnderecos.Remove(IdEndereco);
+        }
+
+    }
+}
